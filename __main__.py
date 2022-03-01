98ddf1f32109f28adbd356752cca61b44dcb1aa0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-01 11:58:09 +0900

    Pythonista ui Thema

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import sys
 import pathlib
 
 import ui
-
+from editor import present_themed
 #import pdbg
 
 sys.path.append(str(pathlib.Path.cwd()) + '/pythonista-webview')
@@ -18,28 +18,36 @@ class View(ui.View):
     self.wv.load_url(str(uri))
     self.wv.flex = 'WH'
     self.refresh_webview()
+
     self.add_subview(self.wv)
-    self.set_reload_btn()
+    self.setup_close_btn()
 
-  def will_close(self):
-    #self.refresh_webview()
-    pass
-
-  def set_reload_btn(self):
-    self.close_btn = self.create_btn('iob:ios7_refresh_outline_32')
-    self.close_btn.action = (lambda sender: self.wv.reload())
-    self.right_button_items = [self.close_btn]
-
-  def create_btn(self, icon):
-    btn_icon = ui.Image.named(icon)
-    return ui.ButtonItem(image=btn_icon)
+  def layout(self):
+    _x, _y, _w, _h = self.frame
+    _, _, btn_w, btn_h = self.close_btn.frame
+    self.close_btn.x = (_w * .92) - (btn_w / 2)
+    self.close_btn.y = (_h * .064) - (btn_h / 2)
 
   def refresh_webview(self):
     self.wv.clear_cache()
     #self.wv.reload()
 
+  def setup_close_btn(self):
+    self.close_btn = self.create_btn('iob:ios7_close_32')
+    self.close_btn.action = (lambda sender: self.close())
+    self.add_subview(self.close_btn)
+
+  def create_btn(self, icon):
+    btn_icon = ui.Image.named(icon)
+    return ui.Button(image=btn_icon)
+
 
 if __name__ == '__main__':
   view = View()
-  view.present(style='fullscreen', orientations=['portrait'])
+  present_themed(
+    view,
+    theme_name='Theme09_Editorial',
+    style='fullscreen',
+    hide_title_bar=True,
+    orientations=['portrait'])
 
